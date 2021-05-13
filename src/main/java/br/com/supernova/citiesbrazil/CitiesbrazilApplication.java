43b77dad7f1c01d8073f0ef254430154fa90d165
William Derek Dias <williamdkdevops@gmail.com>
2021-05-13 18:34:51 -0300

    First Commit

---
+++ src/main/java/br/com/supernova/citiesbrazil/CitiesbrazilApplication.java
@@ -0,0 +1,13 @@
+package br.com.supernova.citiesbrazil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesbrazilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesbrazilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/supernova/citiesbrazil/CitiesbrazilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.supernova.citiesbrazil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesbrazilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
