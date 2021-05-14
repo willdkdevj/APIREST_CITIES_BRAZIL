14a29dabe74ad3228eceb280532b0a9bb118228a
William Derek Dias <williamdkdevops@gmail.com>
2021-05-13 22:30:44 -0300

    Implements the models classes (Contry, State, City, PointType)

---
+++ src/main/java/br/com/supernova/citiesbrazil/model/City.java
@@ -0,0 +1,42 @@
+package br.com.supernova.citiesbrazil.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import org.springframework.data.geo.Point;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "City")
+@Table(name = "cidade")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TypeDefs(value = {
+        @TypeDef(name = "point", typeClass = PointType.class)
+})
+public class City {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private Integer uf;
+
+    private Integer ibge;
+
+    @Column(name = "lat_lon")
+    private String geolocation;
+
+    @Type(type = "point")
+    @Column(name = "lat_lon", updatable = false, insertable = false)
+    private Point location;
+}
---
+++ src/main/java/br/com/supernova/citiesbrazil/model/State.java
@@ -0,0 +1,40 @@
+package br.com.supernova.citiesbrazil.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "State")
+@Table(name = "estado")
+@Data
+@NoArgsConstructor
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
+public class State {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private String uf;
+
+    private Integer ibge;
+
+    @ManyToOne
+    @JoinColumn(name = "pais", referencedColumnName = "id")
+    private Country country;
+
+    @Type(type = "jsonb")
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "ddd", columnDefinition = "jsonb")
+    private List<Integer> ddd;
+}
