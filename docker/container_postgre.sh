fef783b9e71140924a5fa6f9e8b0e412fa1db75e
William Derek Dias <williamdkdevops@gmail.com>
2021-05-26 21:58:34 -0300

    Implemented city script to insert the database

--- docker/container_postgre.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run --name supernova-citiesBrazilDb -d -p 5432:5432 -e POSTGRES_USER=postgres_supernova_user -e POSTGRES_PASSWORD=supernova_pass -e POSTGRES_DB=citiesBrazil -v $PWD:/tmp postgres
-
-docker exec -it supernova-citiesBrazilDb /bin/bash
